5899da8baaf0dac7cadeb3d4d32c458952f39077
stpdev <saitawngpha@gmail.com>
2023-11-17 13:56:16 +0630

    fix admin page error

--- site/src/commonMain/kotlin/com/saitawngpha/blogmultiplatform/models/Post.kt
+++ site/src/commonMain/kotlin/com/saitawngpha/blogmultiplatform/models/Post.kt
@@ -13,8 +13,8 @@ import com.saitawngpha.blogmultiplatform.models.Category
     @SerialName("_id")
      val id: String = "",
      val author: String = "",
-//     val date: Double = 0.0,
-     val date: Long = 0L,
+     val date: Double = 0.0,
+//     val date: Long = 0L,
      val title: String ,
      val subtitle: String ,
      val thumbnail: String ,
